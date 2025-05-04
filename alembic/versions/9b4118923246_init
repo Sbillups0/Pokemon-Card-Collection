85bc4e4e444cf105e59aa8d89eb4d43e372036db
Sbillups0 <sbillups0@me.com>
2025-05-04 02:10:59 -0700

    test

---
+++ alembic/versions/9b4118923246_init
@@ -0,0 +1,28 @@
+"""create global inventory
+
+Revision ID: 9b4118923246
+Revises:
+Create Date: 2025-03-30 11:23:36.782933
+
+"""
+
+from typing import Sequence, Union
+
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision: str = "9b4118923246"
+down_revision: Union[str, None] = None
+branch_labels: Union[str, Sequence[str], None] = None
+depends_on: Union[str, Sequence[str], None] = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
+
--- src/api/server.py
+++ src/api/server.py
@@ -1,5 +1,5 @@
 from fastapi import FastAPI
-from src.api import carts, catalog, bottler, barrels, admin, info, inventory
+from src.api import inventory #carts, catalog, bottler, barrels, admin, info, 
 from starlette.middleware.cors import CORSMiddleware
 #NOTE FROM SHANE: STILL NEEDS TO BE MODIFIED, CONFUSED AS TO HOW.
 
@@ -7,15 +7,6 @@ description = """
 Buy some cards and stuff, or don't...
 """
 tags_metadata = [
-    {"name": "cart", "description": "Place potion orders."},
-    {"name": "catalog", "description": "View the available potions."},
-    {"name": "bottler", "description": "Bottle potions from the raw magical elixir."},
-    {
-        "name": "barrels",
-        "description": "Buy barrels of raw magical elixir for making potions.",
-    },
-    {"name": "admin", "description": "Where you reset the game state."},
-    {"name": "info", "description": "Get updates on time"},
     {
         "name": "inventory",
         "description": "Get the current inventory of shop and buying capacity.",
