ae3b56032af396944b9617175a739fe728f965b3
Shane Billups <107887362+Sbillups0@users.noreply.github.com>
2025-05-04 02:29:22 -0700

    Update e91d0c24f7d0_create_table.py

--- alembic/versions/e91d0c24f7d0_create_table.py
+++ alembic/versions/e91d0c24f7d0_create_table.py
@@ -14,7 +14,7 @@ import sqlalchemy as sa
 
 # revision identifiers, used by Alembic. 
 revision: str = "e91d0c24f7d0"
-down_revision: Union[str, None] = None
+down_revision: Union[str, None] = "9b4118923246"
 branch_labels: Union[str, Sequence[str], None] = None
 depends_on: Union[str, Sequence[str], None] = None
 
